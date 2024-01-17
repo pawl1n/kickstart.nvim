5ad5fea07b3f91e8563467457aabdd7e64d546bd
Andrii Pinchuk <pinchuk.andryusha310212@gmail.com>
2024-01-17 23:35:58 +0200

    chore: do not automatically open trouble on error

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -15,7 +15,7 @@ return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
-    auto_open = true,  -- automatically open the list when you have diagnostics
+    auto_open = false, -- automatically open the list when you have diagnostics
     auto_close = true, -- automatically close the list when you have no diagnostics
   },
 }
