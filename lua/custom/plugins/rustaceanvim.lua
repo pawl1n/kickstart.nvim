5caafb95d6b64fa1fbcfbad693df49c97061fd01
Andrii Pinchuk <pinchuk.andryusha310212@gmail.com>
2024-02-29 01:26:39 +0200

    remove clippy

--- lua/custom/plugins/rustaceanvim.lua
+++ lua/custom/plugins/rustaceanvim.lua
@@ -1,15 +1,3 @@
-vim.g.rustaceanvim = {
-  server = {
-    settings = {
-      ['rust-analyzer'] = {
-        checkOnSave = {
-          command = 'clippy',
-        },
-      },
-    },
-  },
-}
-
 return {
   'mrcjkb/rustaceanvim',
   version = '^4', -- Recommended
