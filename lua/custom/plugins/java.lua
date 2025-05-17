af344484067155d7290517879848aecdc8585445
Andrii <andrii.pinch@gmail.com>
2025-05-17 16:34:02 +0300

    java

--- lua/custom/plugins/java.lua
+++ lua/custom/plugins/java.lua
@@ -1,1 +1,86 @@
-return { 'nvim-java/nvim-java' }
+return {
+  'mfussenegger/nvim-jdtls',
+  ft = { 'java' },
+  config = function()
+    local root_markers = { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle', '.project' }
+    local root_dir = require('jdtls.setup').find_root(root_markers)
+    -- calculate workspace dir
+    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+    local workspace_dir = vim.fn.stdpath 'data' .. '/site/java/workspace-root/' .. project_name
+    vim.fn.mkdir(workspace_dir, 'p')
+    local config = {
+      -- The command that starts the language server
+      -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+      cmd = {
+        'java',
+        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+        '-Dosgi.bundles.defaultStartLevel=4',
+        '-Declipse.product=org.eclipse.jdt.ls.core.product',
+        '-Dlog.protocol=true',
+        '-Dlog.level=ALL',
+        '-javaagent:' .. vim.fn.expand '$MASON/share/jdtls/lombok.jar',
+        '-Xms1g',
+        '--add-modules=ALL-SYSTEM',
+        '--add-opens',
+        'java.base/java.util=ALL-UNNAMED',
+        '--add-opens',
+        'java.base/java.lang=ALL-UNNAMED',
+        '-jar',
+        vim.fn.expand '$MASON/share/jdtls/plugins/org.eclipse.equinox.launcher.jar',
+        '-configuration',
+        vim.fn.expand '$MASON/share/jdtls/config',
+        '-data',
+        workspace_dir,
+      },
+      root_dir = root_dir,
+      settings = {
+        java = {
+          eclipse = { downloadSources = true },
+          configuration = { updateBuildConfiguration = 'interactive' },
+          maven = { downloadSources = true },
+          implementationsCodeLens = { enabled = true },
+          referencesCodeLens = { enabled = true },
+          inlayHints = { parameterNames = { enabled = 'all' } },
+          signatureHelp = { enabled = true },
+          completion = {
+            favoriteStaticMembers = {
+              'org.hamcrest.MatcherAssert.assertThat',
+              'org.hamcrest.Matchers.*',
+              'org.hamcrest.CoreMatchers.*',
+              'org.junit.jupiter.api.Assertions.*',
+              'java.util.Objects.requireNonNull',
+              'java.util.Objects.requireNonNullElse',
+              'org.mockito.Mockito.*',
+            },
+          },
+          sources = {
+            organizeImports = {
+              starThreshold = 9999,
+              staticStarThreshold = 9999,
+            },
+          },
+        },
+      },
+
+      -- Language server `initializationOptions`
+      -- You need to extend the `bundles` with paths to jar files
+      -- if you want to use additional eclipse.jdt.ls plugins.
+      --
+      -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+      --
+      -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+      init_options = {
+        bundles = {},
+      },
+    }
+    -- This starts a new client & server,
+    -- or attaches to an existing client & server depending on the `root_dir`.
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'java' },
+      desc = 'Setup jdtls',
+      callback = function()
+        require('jdtls').start_or_attach(config)
+      end,
+    })
+  end,
+}
