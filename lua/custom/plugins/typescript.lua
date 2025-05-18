49f0b6e6756d3669cd6c507e725d0a05b01ab7e2
Andrii <andrii.pinch@gmail.com>
2025-05-18 09:31:16 +0300

    fix: use indents from buffer settings

--- lua/custom/plugins/typescript.lua
+++ lua/custom/plugins/typescript.lua
@@ -1,5 +1,42 @@
 return {
   'pmizio/typescript-tools.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-  opts = {},
+  opts = {
+    settings = {
+      -- Provide any other default format options you want
+      tsserver_file_preferences = {
+        includeInlayParameterNameHints = 'all',
+        includeCompletionsForModuleExports = true,
+        quotePreference = 'auto',
+      },
+    },
+    on_attach = function(client, bufnr)
+      -- Apply buffer-local indentation settings to tsserver
+      local function apply_buffer_indent_settings()
+        local shiftwidth = vim.bo[bufnr].shiftwidth
+        local tabstop = vim.bo[bufnr].tabstop
+        local expandtab = vim.bo[bufnr].expandtab
+
+        local indent = shiftwidth > 0 and shiftwidth or tabstop
+
+        client.config.settings.tsserver_format_options = {
+          insertSpaceAfterFunctionKeywordForAnonymousFunctions = true,
+          convertTabsToSpaces = expandtab,
+          tabSize = indent,
+          indentSize = indent,
+        }
+
+        client.notify('workspace/didChangeConfiguration', {
+          settings = client.config.settings,
+        })
+      end
+
+      -- Apply settings on attach
+      apply_buffer_indent_settings()
+
+      vim.api.nvim_create_autocmd('OptionSet', {
+        pattern = 'shiftwidth,tabstop,expandtab',
+        callback = apply_buffer_indent_settings,
+      })
+    end,
+  },
 }
